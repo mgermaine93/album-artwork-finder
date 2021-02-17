972e0ac562d1b27ddd2a13d4d729487d47ea38a6
mgermaine93 <mgermaine93@gmail.com>
2021-02-17 18:38:15 -0500

    Updated README with user stories/ideas.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Album Artwork Finder
 
-Details TBD
+Basic Artwork grabber is functional for now. Next big-picture steps/user stories include:
+
+- Perform the current functionality, but with:
+  - Multiple hard-coded search terms.
+  - User-inputted search term(s).
+  - Search terms retrieved from a file that is provided (ultimate goal).
+- Perform the current functionality, but have the program:
+  - Retrieve a handful of results.
+  - Pick the best one (how to do this?):
+    - Image quality.
+    - Image size.
+    - Appropriateness of image.
+- Perform the current functionality, but have the program:
+  - Save the retrieved image as the artwork to the real music file.
+    - Access metadata to do this?
+- Develop minimalist front-end component?
+  - Drag-and-drop feature?
--- artwork-grabber.py
+++ artwork-grabber.py
@@ -2,6 +2,7 @@
 # given the keys specified below and save the artwork to a folder as viewable file.
 
 # Import Selenium, WebDriver, and Time (for sleep)
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -10,6 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 import urllib.request
 import time
+from time import sleep
 
 PATH = "/Users/mgermaine93/Desktop/CODE/album-artwork-finder/chromedriver"
 
@@ -29,10 +31,11 @@ driver.get("https://www.google.com/imghp?hl=en&ogbl")
 # "q" is the name of the google search field input
 search_bar = driver.find_element_by_name("q")
 
+search_term = "Ben Folds Five The Sound of the Live of the Mind Album Cover"
+
 # Input the search term(s)
 # This will need to be dynamic, too
-search_bar.send_keys(
-    "Dave Matthews Band Before These Crowded Streets Album Cover")
+search_bar.send_keys(search_term)
 
 # Returns the results (basically clicks "search"?)
 search_bar.send_keys(Keys.RETURN)
@@ -57,7 +60,7 @@ try:
     # (From SO post) Read the dataURL and decode it to bytes
     with urllib.request.urlopen(data_url) as response:
         data = response.read()
-        with open(f"{save_folder}/dmb.jpg", mode="wb") as f:
+        with open(f"{save_folder}/image.jpg", mode="wb") as f:
             f.write(data)
 
     # This will print if the above succeeds
