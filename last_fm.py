2b5367ad3eeaa63f10e30b1046dede4b55321334
mgermaine93 <mgermaine93@gmail.com>
2022-01-28 15:14:28 -0500

    Messing around with API calls some more.

--- last_fm.py
+++ last_fm.py
@@ -25,40 +25,4 @@ def get_artwork_through_last_fm(artist, album, api_key=API_KEY, api_secret=API_S
     return image
 
 
-print(get_artwork_through_last_fm("Bill Evans", "For Lovers"))
-
-# payload = {
-#     "api_key": API_KEY,
-#     "method": "auth.getToken",
-#     "format": "json"
-# }
-
-# # Fetches a request token
-# token_response = requests.get(
-#     "https://ws.audioscrobbler.com/2.0/", headers=headers, params=payload)
-# print(token_response.status_code)
-
-# # request_token = token_response.json()["token"]
-
-# # Requests authorization from the user -- will probably open a browser
-# authorization_url = f"http://www.last.fm/api/auth/?api_key={API_KEY}&token={request_token}"
-# authorization_response = requests.get(authorization_url, headers=headers)
-
-# # Constructs the API signature
-# api_signature = md5(
-#     f"api_key{API_KEY}methodauth.getSessiontoken{request_token}{SHARED_SECRET}".encode('utf-8')).hexdigest()
-
-# # Fetches the web service session
-# web_service_session_url = f'http://ws.audioscrobbler.com/2.0/?api_key={API_KEY}&token={request_token}&api_sig={api_signature}&method=auth.getSession&format=json'
-# web_service_response = requests.get(
-#     web_service_session_url, headers=headers).json()
-# # print(web_service_response)
-
-
-# # api_signature = md5("api_key={API_KEY}")
-# # # stuff = f'http://ws.audioscrobbler.com/2.0/?method=auth.getToken&api_key={API_KEY}&format=json'
-# # new_stuff = f'http://ws.audioscrobbler.com/2.0/?api_key={API_KEY}&method=auth.getToken&auth.getSession&format=json'
-
-
-# # print(os.getenv("PATH"))
-# # print(os.getenv("API_KEY"))
+print(get_artwork_through_last_fm("Dave Matthews Band", "Crash"))
--- musicbrainz.py
+++ musicbrainz.py
@@ -1,3 +1,4 @@
+from turtle import title
 import musicbrainzngs
 import os
 from dotenv import load_dotenv
@@ -14,13 +15,51 @@ user_agent = musicbrainzngs.set_useragent(
     contact="mgermaine93@gmail.com"
 )
 
+
+def get_artist_id(artist):
+    results = musicbrainzngs.search_artists(artist=artist, limit=5)
+    for artist_info_set in results['artist-list']:
+        if artist_info_set['name'].lower() == artist.lower():
+            return artist_info_set['id']
+        else:
+            return "Not found"
+
+
+def get_releases_by_artist(artist):
+    titles_and_ids_dict = {}
+    artist_id = get_artist_id(artist)
+    releases = musicbrainzngs.browse_releases(
+        artist=artist_id,
+        # limit=30,
+        # release_type=['album', 'single', 'ep',
+        #               'compilation', 'soundtrack', 'live'],
+        # release_status=['official']
+        # release_type=musicbrainzngs.musicbrainz.VALID_RELEASE_TYPES,
+        # release_status=musicbrainzngs.musicbrainz.VALID_RELEASE_STATUSES
+    )
+    for release in releases["release-list"]:
+        titles_and_ids_dict[release["title"].lower()] = release["id"]
+    return titles_and_ids_dict
+
+
+def get_cover_art_with_musicbrainz(artist, album):
+    # artist_id = get_artist_id(artist)
+    album_id = get_releases_by_artist(artist)[album.lower()]
+    return musicbrainzngs.get_image_list(releaseid=album_id)
+
+
+#print(get_artist_id("Ben Folds"))
+print(get_releases_by_artist("Ben Folds"))
+print(get_cover_art_with_musicbrainz(
+    artist="Ben Folds", album="Rockin' the Suburbs"))
+
 # results = musicbrainzngs.get_artist_by_id(
 #     id="51b7f46f-6c0f-46f2-9496-08c9ec2624d4", includes=["recordings", "releases", "works"])
 # print(results)
 
-image_list = musicbrainzngs.get_image_list(
-    "7491bf54-68e7-452e-8b27-d9f73e085101")
-print(image_list)
+# image_list = musicbrainzngs.get_image_list(
+#     "7491bf54-68e7-452e-8b27-d9f73e085101")
+# print(image_list)
 
 # results = musicbrainzngs.search_artists(artist="ben folds")
 # print(results)
